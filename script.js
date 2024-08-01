303be853f1fbc23825c04ba5265dda88b1cd6bef
javohir1606 <javohirallayorov1606@gmail.com>
2024-08-02 02:01:00 +0500

    v1

---
+++ script.js
@@ -0,0 +1,110 @@
+// let a = 1000;
+// let b = 100;
+// console.log(a / b);
+
+// let a = 10;
+// let b = 5;
+// console.log(a / b);
+
+// let a = 7;
+// let b = 5;
+// console.log(a % b);
+
+// let a = 52;
+// let b = 10;
+// console.log((a-(a%b))/b);
+// console.log(a % b);
+
+// let a = 52;
+// let b = a % 10;
+//  let c = (a - b)/10;
+// console.log(b+c);
+
+// let a = 99;
+// let b = a % 10;
+// let c = (a - b) / 10;
+// let e = (b * 10) + c;
+// console.log(e);
+
+// let a = 399;
+// let b = a % 100;
+// let c =(a - b)/ 100;
+// console.log(c); 
+
+// let a = 398;
+// let b = a % 100;
+// let c = b % 10;
+// let d =(b - c)/10;
+// console.log(d);
+// console.log(c);
+// oxirgi ikkita soni chiqarish
+
+// let a = 347;
+// let b = a % 100;
+// let c = b % 10;
+// let d =(b - c)/10;
+// let y =(a - b)/ 100;
+// console.log(c+d+y);
+
+// let a = 347;
+// let b = a % 100;
+// let c = b % 10;
+// let d = (b - c)/10;
+// let e = (a-b)/100;
+// let g = ((c*100)+(d*10)+e);
+// console.log(g);
+
+
+
+
+// Uygi-vazifa
+// 1-misol
+// let num = 321;
+// let a = num % 10; //9
+// let b = (num % 100); // 39
+// let c = b - a; // 30
+// let d = num - b; //200
+// let e = (c / 10); // 3
+// let f = d / 10; // 20
+// let j = a * 100+f+e;
+// console.log(j); 
+
+// 2 - misol
+// let num = 123; 
+// let a = num % 10; // 3
+// let b = num % 100; // 23
+// let c = b - a; // 20
+// let e = c * 10; // 200
+// let f = num % 1000;
+// let d = (f - b)/10;
+// let j = d + a; 
+// let g = e + j;
+// console.log(g);
+
+// 3 - misol 132
+// let num = 123; 
+// let a = num % 10; // 3
+// let b = a*10// 30
+// let c = num % 100 // 23
+// let d = (c - a)/ 10; // 2
+// let g = b + d // 32
+// let h = num % 1000; // 123
+// let j = h - c 
+// let m = j + g
+// console.log(m);
+
+// 4 - misol
+// let num = 1624;
+// let a = num % 100; //24
+// let b = a%10
+// let c = (a-b)/10
+// console.log(c);
+
+
+// 5 - misol
+// let num = 1624;
+// let a = num %1000 // 624
+// let b = num % 100 // 24
+// let c  = (a - b)/100
+// console.log(c);
+
